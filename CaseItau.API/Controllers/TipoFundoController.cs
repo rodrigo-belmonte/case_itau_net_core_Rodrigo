0ef8a2743cd086413fe4c136848dcd3b99fbbc4f
rodrigo-belmonte <rodrigo.belmonte@outlook.com>
2024-06-21 16:39:22 -0300

    Criação do Endpoint TipoFundo para Listar todos os Tipos

---
+++ CaseItau.API/Controllers/TipoFundoController.cs
@@ -0,0 +1,31 @@
+﻿using CaseItau.Application.Services.TipoFundo;
+using CaseItau.Domain.Reponses.Fundo;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace CaseItau.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TipoFundoController : ControllerBase
+    {
+        private readonly ITipoFundoService _tipoFundoService;
+
+        public TipoFundoController(ITipoFundoService tipoFundoService)
+        {
+            _tipoFundoService = tipoFundoService;
+        }
+        [HttpGet]
+        public async Task<ActionResult<GetListFundosTipoFundoResponse>> Get()
+        {
+            var response = await _tipoFundoService.GetAllTiposFundo();
+            if (!response.Success)
+            {
+                return BadRequest(response);
+            }
+
+            return Ok(response);
+        }
+
+    }
+}
