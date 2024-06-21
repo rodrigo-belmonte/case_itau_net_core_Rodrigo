803281cd1c5b2739910aa906e7b611b1f81e4a66
rodrigo-belmonte <rodrigo.belmonte@outlook.com>
2024-06-21 16:41:20 -0300

    Testes Unitários

---
+++ CaseItau.UnitTests/Application/FundoService/FundoServiceBaseFixture.cs
@@ -0,0 +1,107 @@
+﻿using Bogus;
+using Bogus.Extensions.Brazil;
+using CaseItau.Domain.Repository;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaseItau.UnitTests.Application.FundoService
+{
+    public class FundoServiceBaseFixture
+    {
+        public Faker Faker { get; set; }
+
+        protected FundoServiceBaseFixture()
+            => Faker = new Faker("pt_BR");
+
+        public bool GetRandomBoolean()
+            => new Random().NextDouble() < 0.5;
+
+        public Mock<IFundoRepository> GetFundoRepositoryMock() => new Mock<IFundoRepository>();
+        public Mock<ITipoFundoRepository> GetTipoFundoRepositoryMock() => new Mock<ITipoFundoRepository>();
+
+
+        public Domain.Entity.Fundo GetInput()
+        {
+            return GetValidFundo();
+        }
+
+        public Domain.Entity.Fundo GetValidFundo()
+        {
+            return new Domain.Entity.Fundo
+            {
+                Codigo = $"ITAU{Faker.Random.String(2)}{Faker.Random.Int(100, 999)}",
+                Cnpj = Faker.Company.Cnpj(),
+                Nome = Faker.Company.CompanyName(),
+                CodigoTipo = Faker.Random.Number(1, 5),
+                Patrimonio = Faker.Random.Decimal()
+            };
+        }
+
+        public Domain.Entity.Fundo GetCnpjNull()
+        {
+            var invalidFundo = GetValidFundo();
+            invalidFundo.Cnpj = null;
+
+            return invalidFundo;
+        }
+        public Domain.Entity.Fundo GetInvalidCnpj()
+        {
+            var invalidFundo = GetValidFundo();
+            invalidFundo.Cnpj = Faker.Random.String2(10, 16, "01234567890");
+
+            return invalidFundo;
+        }
+
+        public Domain.Entity.Fundo GetCnpjAlreadyExist()
+        {
+            var invalidFundo = GetValidFundo();
+            invalidFundo.Cnpj = "12222333444455";
+
+            return invalidFundo;
+        }
+
+        public Domain.Entity.Fundo GetCodigoNull()
+        {
+            var invalidFundo = GetValidFundo();
+            invalidFundo.Codigo = null;
+
+            return invalidFundo;
+        }
+
+        public Domain.Entity.Fundo GetCodigoAlreadyExist()
+        {
+            var invalidFundo = GetValidFundo();
+            invalidFundo.Codigo = "ITAURF123";
+
+            return invalidFundo;
+        }
+
+        public Domain.Entity.Fundo GetCodigoTipoZero()
+        {
+            var invalidFundo = GetValidFundo();
+            invalidFundo.CodigoTipo = 0;
+
+            return invalidFundo;
+        }
+        public Domain.Entity.Fundo GetCodigoTipoNotExist()
+        {
+            var invalidFundo = GetValidFundo();
+            invalidFundo.CodigoTipo = Faker.Random.Int(4, 10);
+
+            return invalidFundo;
+        }
+
+        public Domain.Entity.Fundo GetNomeNull()
+        {
+            var invalidFundo = GetValidFundo();
+            invalidFundo.Nome = null;
+
+            return invalidFundo;
+        }
+
+    }
+}
---
+++ CaseItau.UnitTests/Application/FundoService/FundoServiceTest.cs
@@ -0,0 +1,52 @@
+﻿using AutoMapper;
+using CaseItau.Application.Map;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Xunit;
+using Services = CaseItau.Application.Services.Fundo;
+namespace CaseItau.UnitTests.Application.FundoService
+{
+    public class FundoServiceTest
+    {
+        private readonly FundoServiceBaseFixture _fixture;
+
+        public FundoServiceTest(FundoServiceBaseFixture fixture)
+        {
+            _fixture = fixture;
+        }
+        [Fact(DisplayName = nameof(CreateFundo))]
+        [Trait("Application", "CreateFundo - Services")]
+        public async void CreateFundo()
+        {
+            var fundoRepositoryMock = _fixture.GetFundoRepositoryMock();
+            var tipoFundoRepositoryMock = _fixture.GetTipoFundoRepositoryMock();
+            var myProfile = new MappingProfile();
+            var configuration = new MapperConfiguration(cfg => cfg.AddProfile(myProfile));
+            IMapper mapper = new Mapper(configuration);
+
+            var service = new Services.FundoService(fundoRepositoryMock.Object, tipoFundoRepositoryMock.Object, mapper);
+
+            var input = _fixture.GetInput();
+            var output = await service.CreateFundo(input);
+
+            fundoRepositoryMock.Verify(
+                repository => repository.InsertAsync(input), 
+                Times.Once
+                );
+            output.Fundo.Should().NotBeNull();
+            output.Fundo.Codigo.Should().Be(input.Codigo);
+            output.Fundo.Nome.Should().Be(input.Nome);
+            output.Fundo.Cnpj.Should().Be(input.Cnpj);
+            output.Fundo.Patrimonio.Should().Be(input.Patrimonio);
+            output.Fundo.CodigoTipo.Should().Be(input.CodigoTipo);
+
+            output.Success.Should().BeTrue();
+            output.Errors.Should().BeEmpty();
+        }
+    }
+}
